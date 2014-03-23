26e632ca735a60d16f56181d8c9922f30fa80802
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-03-23 17:34:04 +0100

    Thesis build script now uncompresses packages and enclosed resources. Be sure to add this functionality to proposal build script. Enclosed resources are now included in a .zip under dv2524-encl/ directory.

--- dv2524-thesis/build.sh
+++ dv2524-thesis/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+clear
+printf "dv2524-thesis/build.sh\n"
+printf "---\n\n"
 
 # Clean:
+printf "Cleaning...\n"
 for file in *.aux *.bbl *.blg *.log *.out ; do
 	rm $file
 done
 
-# Copy required packages to bin:
+# Unzip packages
+printf "Uncompressing...\n"
+unzip -o ../dv2524-packages/gitinfo.zip -d ../dv2524-packages
+unzip -o ../dv2524-encl/dv2524-encl.zip -d ../dv2524-encl
+
+# Move files into build directory:
+printf "Copying files into build directory...\n"
 cp ../dv2524-packages/gitinfo/gitinfo.sty gitinfo.sty
 cp ../dv2524-packages/gitinfo/gitsetinfo.sty gitsetinfo.sty
 
+printf "Building LaTeX-document with pdflatex...\n"
 pdflatex thesis
 pdflatex thesis
 
