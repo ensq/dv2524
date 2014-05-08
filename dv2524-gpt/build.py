66fb089ef10f09a14442a816dc58eff871394606
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-05-08 11:02:36 +0200

    dv2524-gpt/build.py now processes collected data of variations in-between modified key-values in Simics software rasterizer and paravirtualization data chain.

--- dv2524-gpt/build.py
+++ dv2524-gpt/build.py
@@ -8,13 +8,28 @@ g_iam = "dv2524-gpt/build.py"
 # Entry point:
 print(g_iam + ": Enter...")
 
-cmd = "gnuplot -e \"arg_data1='%s';arg_data2='%s';arg_data3='%s'\" demo.gpt";
+# TODO: Delete old plots.
+
+benchmarks = \
+	[["julia225.ms","julia450.ms","julia900.ms"],\
+	["phong1448x1448.ms", "phong2048x2048.ms", "phong2896x2896.ms"],\
+	["chess60x60.ms", "chess84x84.ms", "chess118x118.ms"]]
+platforms = ["simics", "para"]
 
 arg1 = "results/paraphong1448x1448.ms"
 arg2 = "results/paraphong2048x2048.ms"
 arg3 = "results/paraphong2896x2896.ms"
-for i in range(0,10):
-	sp = subprocess.Popen(cmd % (arg1, arg2, arg3), shell=True)
-	sp.wait()
+arg4 = "snerpa"
+
+cmd = "gnuplot -e \"arg_data1='%s';arg_data2='%s';arg_data3='%s';arg_output='%s'\" demo.gpt";
+for platform in platforms:
+	for benchmark in benchmarks:
+		arg1 = "results/" + platform + benchmark[0]
+		arg2 = "results/" + platform + benchmark[1]
+		arg3 = "results/" + platform + benchmark[2]
+		arg4 = platform + benchmark[0] + ".png"
+		
+		sp = subprocess.Popen(cmd % (arg1, arg2, arg3, arg4), shell=True)
+		sp.wait()
 
 print(g_iam + ": Exit.")
