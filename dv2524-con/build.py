bac555b8d4fa52b1af114aff016b87a591096db2
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-05-20 16:20:53 +0200

    Removed overcomplicated handling of placeholder resources in dv2524-con/build.py.

--- dv2524-con/build.py
+++ dv2524-con/build.py
@@ -10,13 +10,8 @@ g_iam = "dv2524-con/build.py"
 # Entry point:
 print(g_iam + ": Enter...")
 
-if not os.path.isfile("Intel-logo.pdf"):
-    print(g_iam + ": Warning - No Intel Logo present. Using placeholder image.")
-    shutil.copyfile("pdficon_large.pdf", "Intel-logo.pdf")
-
 srcDir = os.listdir(".")
 dstDir = "../dv2524-bin/"
-
 for files in srcDir:
 	if not files.endswith(".py"):
 		shutil.copy(files, dstDir)
