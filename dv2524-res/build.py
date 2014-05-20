94f26b8fda1da2e118abf348ccf5811ef1f8cfb5
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-05-20 16:19:09 +0200

    No longer redundantly compressing files present in dv2524-res/ directory.

--- dv2524-res/build.py
+++ dv2524-res/build.py
@@ -5,25 +5,12 @@ import os
 import sys
 import getopt
 import shutil
-import zipfile
 import distutils.core
 
 g_iam = "dv2524-res/build.py"
 
 def usage(p_from):
   print(p_from + ": Usage: " + sys.argv[0] + " -t TARGET")
-
-def unzipToDir(p_from, p_source, p_target):
-    print(p_from + ": Unzipping " + p_source + " to " + p_target + " ...")
-    with zipfile.ZipFile(p_source, 'r') as zip:
-      zip.extractall(p_target)
-
-def clean(p_from, p_dir):
-    print(p_from + ": Deleting " + p_dir +"/ directory...")
-    try:
-        shutil.rmtree("./" + p_dir)
-    except Exception: # I.e. not found.
-        pass
 
 # Entry point:
 print(g_iam + ": Enter...")
@@ -47,14 +34,11 @@ if len(opts)<1:
     usage(g_iam)
     sys.exit(2)
 
+buildDirectory = "../dv2524-bin/"
 if target=="thesis":
-    unzipToDir(g_iam, "dv2524-res-thesis.zip", ".")
-    distutils.dir_util.copy_tree("./dv2524-res-thesis", "../dv2524-bin")
-    clean(g_iam, "dv2524-res-thesis")
+    shutil.copy2("thesisindexstyle.ist", buildDirectory)
 elif target=="proposal":
-    unzipToDir(g_iam, "dv2524-res-proposal.zip", ".")
-    distutils.dir_util.copy_tree("./dv2524-res-proposal", "../dv2524-bin")
-    clean(g_iam, "dv2524-res-proposal")
+    shutil.copy2("androidopenglesdesignoverview.pdf", buildDirectory)
 else:
     usage(g_iam)
     sys.exit(2)
