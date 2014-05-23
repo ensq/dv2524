2776aa378d0424a3893c33df85113d441183097d
Eric Nilsson <CaterHatterPillar@gmail.com>
2014-05-23 14:19:07 +0200

    Added outline for presentation.

--- dv2524-pre/build.py
+++ dv2524-pre/build.py
@@ -26,6 +26,8 @@ subprocess.call(["git", "checkout"])
 
 print("Building LaTeX-document with pdflatex...")
 subprocess.call("pdflatex presentation", shell=True)
+subprocess.call("pdflatex presentation", shell=True)
+subprocess.call("pdflatex presentation", shell=True)
 
 # When build has completed, reset working directory back to original directory:
 os.chdir("../dv2524-pre/")
